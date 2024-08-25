dc637141d3f39ec0bc6e47351c1e56e7a5d3dfbb
anglaevaos <anglaevaos@gmail.com>
2024-08-25 15:46:32 +0300

    feat: добавить Keyboard

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -3,18 +3,18 @@ import UIKit
 
 class LogInViewController: UIViewController {
     
+    private let notificationCenter = NotificationCenter.default
+    
     private lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.showsVerticalScrollIndicator = true
         scrollView.showsHorizontalScrollIndicator = false
-        view.backgroundColor = .systemCyan
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         return scrollView
     }()
     
     private lazy var contentView: UIView = {
         let contentView = UIView()
-        contentView.backgroundColor = .red
         contentView.translatesAutoresizingMaskIntoConstraints = false
         return contentView
     }()
@@ -41,6 +41,7 @@ class LogInViewController: UIViewController {
         emailPhoneTextField.layer.borderColor = UIColor.lightGray.cgColor
         emailPhoneTextField.layer.cornerRadius = 10
         emailPhoneTextField.layer.borderWidth = 0.5
+        
         emailPhoneTextField.layer.masksToBounds = true
         emailPhoneTextField.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         emailPhoneTextField.backgroundColor = UIColor.systemGray6
@@ -51,6 +52,7 @@ class LogInViewController: UIViewController {
         emailPhoneTextField.clearButtonMode = UITextField.ViewMode.whileEditing
         emailPhoneTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
         emailPhoneTextField.translatesAutoresizingMaskIntoConstraints = false
+        
         return emailPhoneTextField
     }()
     
@@ -88,10 +90,12 @@ class LogInViewController: UIViewController {
         button.layer.cornerRadius = 10
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.backgroundColor?.withAlphaComponent(0.8)
         button.setBackgroundImage(UIImage(named: "blue_pixel"), for: .normal)// уточнить
         button.setBackgroundImage(UIImage(named: "blue_pixel"), for: .disabled)//уточнить
         button.setBackgroundImage(UIImage(named: "blue_pixel"), for: .highlighted)//уточнить
         button.setBackgroundImage(UIImage(named: "blue_pixel"), for: .selected)//уточнить
+        
         button.addTarget(self, action: #selector(buttonTappedToProfile), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -101,83 +105,106 @@ class LogInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
-        setupScrollView()
-        setupUI()
-        addConstraint()
+        setupLayout()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: animated)
-    }
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            super.viewWillDisappear(animated)
+            notificationCenter.addObserver(self,selector: #selector(keyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+            notificationCenter.addObserver(self,selector: #selector(keyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: animated)
-    }
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewDidDisappear(animated)
+            notificationCenter.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+            notificationCenter.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
     
-    private func setupScrollView() {
+        @objc private func keyboardShow(notification: NSNotification) {
+            if let keyboardHeight = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height {
+    
+                scrollView.contentInset.bottom = keyboardHeight
+                scrollView.verticalScrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+            }
+        }
+    
+        @objc private func keyboardHide() {
+            scrollView.contentInset = .zero
+            scrollView.verticalScrollIndicatorInsets = .zero
+        }
+
+    
+    
+    private func setupLayout() {
         view.addSubview(scrollView)
+                
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
         scrollView.addSubview(contentView)
-    }
-    
-    
-    private func setupUI() {
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        
         contentView.addSubview(logoImageView)
         contentView.addSubview(logInButton)
         contentView.addSubview(emailPhoneTextField)
         contentView.addSubview(passwordTextField)
-    }
-    
-    
-    private func addConstraint(){
+        
         NSLayoutConstraint.activate([
-            logoImageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120),
-            logoImageView.widthAnchor.constraint(equalToConstant: 100),
-            logoImageView.heightAnchor.constraint(equalToConstant: 100),
             
-            emailPhoneTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.centerXAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.centerXAnchor),
+            logoImageView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 220),
+            logoImageView.heightAnchor.constraint(equalToConstant: 100),
+            logoImageView.widthAnchor.constraint(equalToConstant: 100),
+            
+            
+            emailPhoneTextField.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             emailPhoneTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 120.5),
             emailPhoneTextField.heightAnchor.constraint(equalToConstant: 50),
-            emailPhoneTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),//отступ слева 16
-            emailPhoneTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            emailPhoneTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),//отступ слева 16
+            emailPhoneTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             
-            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            passwordTextField.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             passwordTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 170),
             passwordTextField.heightAnchor.constraint(equalToConstant: 50),
-            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),//отступ слева 16
-            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            passwordTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),//отступ слева 16
+            passwordTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             
-            logInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            logInButton.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 236),
+            
+            logInButton.leadingAnchor.constraint(equalTo: passwordTextField.leadingAnchor),
+            logInButton.trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor),
+            logInButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 16),
             logInButton.heightAnchor.constraint(equalToConstant: 50),
-            logInButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),//отступ слева 16
-            logInButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            logInButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
             
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        
-        // Установка ограничений для contentView
-            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 1000),
-            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
         ])
     }
     
-    
-    @objc private func buttonTappedToProfile() {
-           let profileVC = ProfileViewController()
-           navigationController?.pushViewController(profileVC, animated: true)
-       }
 
+    @objc private func buttonTappedToProfile() {
+        let profileVC = ProfileViewController()
+        navigationController?.pushViewController(profileVC, animated: true)
+    }
     
 }
+
+
+
+
+
+
+
 
 
 
