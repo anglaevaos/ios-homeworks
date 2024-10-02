b73ac94afb1f57843f9b9511f0af9c62624ee56e
anglaevaos <anglaevaos@gmail.com>
2024-10-02 21:54:02 +0300

    feat: добавить class User

---
+++ Navigation/Profile/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  Navigation
+//
+//  Created by Oksana Anglaeva on 02.10.2024.
+//
+
+import UIKit
+
+class User: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
